37f4e4f457e80dceaec2746c45a47b901367cec1
Morten Fyhn Amundsen <morten.fyhn.amundsen@gmail.com>
2015-01-16 12:33:51 +0100

    Oppgavesvar og thread i C

---
+++ Exercise1/thread_test.c
@@ -0,0 +1,36 @@
+#include <pthread.h>
+#include <stdio.h>
+
+int main(void)
+{
+	static int i = 0;
+
+	pthread_t thread_1;
+	pthread_create(&thread_1, NULL, thread_1_foo, NULL);
+
+	pthread_t thread_2;
+	pthread_create(&thread_2, NULL, thread_2_foo, NULL);
+
+	pthread_join(thread_1, NULL);
+	pthread_join(thread_2, NULL);
+
+	printf("%d\n", i);
+
+	return 0;
+}
+
+void thread_1_foo(void)
+{
+	for (int j = 0; j < 1000000; ++j)
+	{
+		i++;
+	}
+}
+
+void thread_2_foo(void)
+{
+	for (int j = 0; j < 1000000; ++j)
+	{
+		i--;
+	}
+}
