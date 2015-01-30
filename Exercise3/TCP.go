f82880f74e2292b6fd18bcb9071dc7ead8c687d5
oyvindwilson <oyvinwil@itk-d651>
2015-01-30 12:56:32 +0100

    TCP tested and works

--- Exercise3/TCP.go
+++ Exercise3/TCP.go
@@ -19,10 +19,15 @@ func main() {
 	socket, err := net.DialTCP("tcp", nil, raddr)
 	if err != nil {log.Fatal(err)}
 
-	var buffer[32] byte
+	var buffer[64] byte
+	var msg = "I am the walrus\x00"
 
 	for {
 		_, err := socket.Read(buffer[:])
+		if err != nil {log.Fatal(err)}
+		log.Println(string(buffer[:]))
+		
+		_, err = socket.Write([]byte(msg))
 		if err != nil {log.Fatal(err)}
 
 		time.Sleep(500*time.Millisecond)
