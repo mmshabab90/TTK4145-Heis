a81d408c2f4bdbc0d2b01ea330d7e0d903a2feed
Morten Fyhn Amundsen <morten.fyhn.amundsen@gmail.com>
2015-02-06 17:28:19 +0100

    Revert "Revert "changed function name""
    This reverts commit ea4e1ec44bfd0e2b612b81906bcfcfa71e84108d.

--- Exercise4/tcp_connect.go
+++ Exercise4/tcp_connect.go
@@ -13,7 +13,7 @@ func ConnectToSlavesByTcp(ipAddresses []string) {
 	// ! validIpList(ipAddresses) -> error!
 	
 	sort.Strings(ipAddresses)
-	if !sort.	(ipAddresses) {log.Fatal("IP addresses not sorted!")}
+	if !sort.StringsAreSorted(ipAddresses) {log.Fatal("IP addresses not sorted!")}
 
 	masterIp := ipAddresses[0]
 	// masterIp != getOwnIp() -> error!
@@ -37,6 +37,35 @@ func sortIpsAscending(ipAddresses []string) []string {
 	return ipAddresses
 }
 
+func padIpAddress(ipAddress string) string {
+	// check valid ip
+	var paddedIp string
+	var index int
+	for i := 0; i < 3; i++ {
+		index = strings.Index(ipAddress, ".")
+		if index == 1 {
+			paddedIp += "00" + ipAddress[:2]
+			ipAddress = ipAddress[2:]
+		} else if index == 2 {
+			paddedIp += "0" + ipAddress[:3]
+			ipAddress = ipAddress[3:]
+		}
+	}
+
+	index = strings.Index(ipAddress, ":")
+	if index == 1 {
+			paddedIp += "00" + ipAddress[:1]
+		} else if index == 2 {
+			paddedIp += "0" + ipAddress[:2]
+	}
+
+
+	return paddedIp
+
+	// for hver av de fire segmentene:
+		// stapp inn riktig antall nuller (index-3)
+}
+
 func get4thIpByte(ipAddress string) int {
 	loIndex := strings.LastIndex(ipAddress, ".")
 	hiIndex := strings.Index(ipAddress, ":")
@@ -48,14 +77,12 @@ func get4thIpByte(ipAddress string) int {
 }
 
 func main() {
-	rawIps := []string{"129.241.187.16:20014",
+	rawIps := []string{"5.41.7.16:20014",
 					   "129.01.187.57:9354",
 					   "129.241.187.225:3687",
 					   "129.2.187.79:10784",
-					   "129.37.187.0:41634"}
-
-	for i := 0; i < len(rawIps); i++ {
-		fmt.Println(get4thIpByte(rawIps[i]))
-	}
+					   "29.37.187.0:41634"}
+	for i := 
+	fmt.Println(padIpAddress(rawIps[0]))
 
 }
