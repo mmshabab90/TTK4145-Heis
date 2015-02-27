da066f020a69d029b6997fe8c6e9bb0b3e0b0740
Morten Fyhn Amundsen <morten.fyhn.amundsen@gmail.com>
2015-02-27 17:47:25 +0100

    more "elev_driver" to "driver"

--- THE_ELEVATOR/src/main.go
+++ THE_ELEVATOR/src/main.go
@@ -8,48 +8,48 @@ import (
 )
 
 func main(){
-	elev_driver.Elev_init()
-	elev_driver.Elev_set_motor_direction(elev_driver.DIRN_UP)
+	driver.Elev_init()
+	driver.Elev_set_motor_direction(driver.DIRN_UP)
 	
-	log.Println(elev_driver.DIRN_STOP)
+	log.Println(driver.DIRN_STOP)
 	
 	for{
-		floor := elev_driver.Elev_get_floor_sensor_signal()
-		stop := elev_driver.Elev_get_stop_signal()
-		obstr := elev_driver.Elev_get_obstruction_signal()
+		floor := driver.Elev_get_floor_sensor_signal()
+		stop := driver.Elev_get_stop_signal()
+		obstr := driver.Elev_get_obstruction_signal()
 		if floor == 3 {
-			elev_driver.Elev_set_floor_indicator(floor)
-			elev_driver.Elev_set_motor_direction(elev_driver.DIRN_DOWN)
-			for j:= elev_driver.BUTTON_CALL_DOWN; j < 3; j++ {
-				elev_driver.Elev_set_button_lamp(j, floor, false)
+			driver.Elev_set_floor_indicator(floor)
+			driver.Elev_set_motor_direction(driver.DIRN_DOWN)
+			for j:= driver.BUTTON_CALL_DOWN; j < 3; j++ {
+				driver.Elev_set_button_lamp(j, floor, false)
 			}
 		} else if floor == 0 {
-			elev_driver.Elev_set_floor_indicator(floor)
-			elev_driver.Elev_set_motor_direction(elev_driver.DIRN_UP)
-			for j:= elev_driver.BUTTON_CALL_UP; j < 3; j++ {
-				if j == elev_driver.BUTTON_CALL_DOWN{continue}
-				elev_driver.Elev_set_button_lamp(j, floor, false)
+			driver.Elev_set_floor_indicator(floor)
+			driver.Elev_set_motor_direction(driver.DIRN_UP)
+			for j:= driver.BUTTON_CALL_UP; j < 3; j++ {
+				if j == driver.BUTTON_CALL_DOWN{continue}
+				driver.Elev_set_button_lamp(j, floor, false)
 			}
 		} else if floor != -1 {
-			elev_driver.Elev_set_floor_indicator(floor)
-			for j:= elev_driver.BUTTON_CALL_UP; j < 3; j++ {
-				elev_driver.Elev_set_button_lamp(j, floor, false)
+			driver.Elev_set_floor_indicator(floor)
+			for j:= driver.BUTTON_CALL_UP; j < 3; j++ {
+				driver.Elev_set_button_lamp(j, floor, false)
 			}
 		}
-		for j:= elev_driver.BUTTON_CALL_UP; j < 3; j++ {
+		for j:= driver.BUTTON_CALL_UP; j < 3; j++ {
 			for i:=0; i < 4; i++{
-				if i == 3 && j == elev_driver.BUTTON_CALL_UP{continue}
-				if i == 0 && j == elev_driver.BUTTON_CALL_DOWN{continue}
-				button := elev_driver.Elev_get_button_signal(j, i)
+				if i == 3 && j == driver.BUTTON_CALL_UP{continue}
+				if i == 0 && j == driver.BUTTON_CALL_DOWN{continue}
+				button := driver.Elev_get_button_signal(j, i)
 				if button {
-					elev_driver.Elev_set_button_lamp(j, i, button)
+					driver.Elev_set_button_lamp(j, i, button)
 				}
 			}
 		}
-		elev_driver.Elev_set_stop_lamp(stop)
-		elev_driver.Elev_set_door_open_lamp(obstr)
+		driver.Elev_set_stop_lamp(stop)
+		driver.Elev_set_door_open_lamp(obstr)
 		if(stop){
-			elev_driver.Elev_set_motor_direction(elev_driver.DIRN_STOP)
+			driver.Elev_set_motor_direction(driver.DIRN_STOP)
 		}
 	}
 }
