b4bd80590380d1f787d4e39202cb7eb689834d2e
Morten Fyhn Amundsen <morten.fyhn.amundsen@gmail.com>
2015-02-28 14:58:50 +0100

    gofmt'd the fsm file

--- THE_ELEVATOR/src/fsm/fsm.go
+++ THE_ELEVATOR/src/fsm/fsm.go
@@ -8,29 +8,31 @@ import (
 	"runtime"
 )
 
-var _ = log.Fatal // For debugging only, remove when done
-var _ = reflect.ValueOf // For debugging only, remove when done
+var _ = log.Fatal         // For debugging only, remove when done
+var _ = reflect.ValueOf   // For debugging only, remove when done
 var _ = runtime.FuncForPC // For debugging only, remove when done
 
 type State_t int
+
 const (
 	idle State_t = iota
 	moving
 	doorOpen
 )
 
-var		state			State_t
-var		direction		Elev_motor_direction_t
-var		floor			int
-var		departDirection	Elev_motor_direction_t
-const 	doorOpenTime	= 3.0
+var state State_t
+var direction Elev_motor_direction_t
+var floor int
+var departDirection Elev_motor_direction_t
+
+const doorOpenTime = 3.0
 
 func syncLights() {
 	for f := 0; f < N_FLOORS; f++ {
 		for b := 0; b < N_BUTTONS; b++ {
 			if (b == BUTTON_CALL_UP && f == N_FLOORS-1) ||
-			(b == BUTTON_CALL_DOWN && f == 0) {
-			   	continue
+				(b == BUTTON_CALL_DOWN && f == 0) {
+				continue
 			} else {
 				Elev_set_button_lamp(b, f, Orders_isOrder(f, b))
 			}
@@ -48,29 +50,29 @@ func Init() {
 
 func EventButtonPressed(buttonFloor int, buttonType Elev_button_type_t) {
 	switch state {
-		case idle:
+	case idle:
+		queue.AddOrder(buttonFloor, buttonType)
+		direction = queue.ChooseDirection(floor, direction)
+		if direction == DIRN_STOP {
+			driver.SetDoorOpenLamp(true)
+			queue.RemoveOrdersAt(floor)
+			// timer.Start(doorOpenTime)
+			state = doorOpen
+		} else {
+			driver.SetMotorDirection(direction)
+			state = moving
+			departDirection = direction
+		}
+	case doorOpen:
+		if floor == buttonFloor {
+			// timer.Start(doorOpenTime)
+		} else {
 			queue.AddOrder(buttonFloor, buttonType)
-			direction = queue.ChooseDirection(floor, direction)
-			if direction == DIRN_STOP {
-				driver.SetDoorOpenLamp(true)
-				queue.RemoveOrdersAt(floor)
-				// timer.Start(doorOpenTime)
-				state = doorOpen
-			} else {
-				driver.SetMotorDirection(direction)
-				state = moving
-				departDirection = direction
-			}
-		case doorOpen:
-			if floor == buttonFloor {
-				// timer.Start(doorOpenTime)
-			} else {
-				queue.AddOrder(buttonFloor, buttonType)
-			}
-		case moving:
-			queue.AddOrder(buttonFloor, buttonType)
-		default:
-			// log error invalid state
+		}
+	case moving:
+		queue.AddOrder(buttonFloor, buttonType)
+	default:
+		// log error invalid state
 	}
 	syncLights()
 }
@@ -111,5 +113,3 @@ func EventTimerTimeOut() {
 		// makes no sense
 	}
 }
-
-func main() {}
