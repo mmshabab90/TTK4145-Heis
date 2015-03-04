d60b6045141edd441bfc521023a2085071210a82
Morten Fyhn Amundsen <morten.fyhn.amundsen@gmail.com>
2015-03-04 23:04:15 +0100

    queue.go: prepending

--- THE_ELEVATOR/src/main.go
+++ THE_ELEVATOR/src/main.go
@@ -3,12 +3,12 @@ package main
 import (
 	"./elev"
 	"./fsm"
-	"./queue"
+//	"./queue"
 	"log"
 )
 
 type Keypress struct {
-	button Elev_button_type_t
+	button int
 	floor int
 }
 
@@ -36,32 +36,32 @@ func init() {
 		log.Fatalln("Io_init() failed!")
 	}
 
-	elev.SetMotorDirection(DirnDown)
+	elev.SetMotorDirection(elev.DirnDown)
 	for elev.GetFloor() == -1 {}
-	elev.SetMotorDirection(DirnStop)
+	elev.SetMotorDirection(elev.DirnStop)
 
 	fsm.Init()
 	// Add some error handling here.
 }
 
 func pollKeypresses(c chan Keypress) {
-	var buttonState = [NumFloors][NumButtons]bool{
+	var buttonState = [elev.NumFloors][elev.NumButtons]bool{
 		{false, false, false},
 		{false, false, false},
 		{false, false, false},
-		{false, false, false} // find a better way to do this
+		{false, false, false}, // find a better way to do this
 	}
 
 	for {
-		for f := 0; f < NumFloors; f++ {
-			for b := 0; b < NumButtons; b++ {
-				if (f == 0 && b == ButtonCallDown) ||
-				(f == NumFloors-1 && b == ButtonCallUp) {
+		for f := 0; f < elev.NumFloors; f++ {
+			for b := 0; b < elev.NumButtons; b++ {
+				if (f == 0 && b == elev.ButtonCallDown) ||
+				(f == elev.NumFloors-1 && b == elev.ButtonCallUp) {
 					continue
 				}
 				if elev.GetButton(f, b) {
 					if !buttonState[f][b] {
-						c <- Keypress{button = b, floor = f}
+						c <- Keypress{button: b, floor: f}
 					}
 					buttonState[f][b] = true
 				} else {
