d89a7e36b2cac64d8204c464d5d2ec3b0d3d8494
Morten Fyhn Amundsen <morten.fyhn.amundsen@gmail.com>
2015-03-13 17:15:26 +0100

    made a const and init timer time 0

--- THE_ELEVATOR/src/timer/timer.go
+++ THE_ELEVATOR/src/timer/timer.go
@@ -9,11 +9,11 @@ var _ = log.Fatal // For debugging only, remove when done
 
 var TimerOut = make(chan bool)
 var ResetTimer = make(chan bool)
-var doorOpenTime = 3 * time.Second
+const doorOpenTime = 3 * time.Second
 
 func Init() {
-	timer := time.NewTimer(doorOpenTime)
-	timer.Stop()
+	timer := time.NewTimer(0)
+	//timer.Stop()
 
 	go func() {
 		for {
