01a770080c997ba68866713a01cf85c1fa92b807
Morten Fyhn Amundsen <morten.fyhn.amundsen@gmail.com>
2015-04-10 14:03:31 +0200

    Finished cost function draft (UNTESTED)

--- THE_ELEVATOR/src/cost/cost.go
+++ THE_ELEVATOR/src/cost/cost.go
@@ -8,15 +8,15 @@ type queueEntry struct {
 var sharedQueue [elev.NumFloors][2]queueEntry
 // end does not belong
 
-func calculateCost(targetFloor int, targetDirection elev.DirnType) {
+func calculateCost(targetFloor int, targetDirection elev.DirnType) int {
 	cost := 0
-
-	// legg til 1 cost om heisen er mellom etasjer:
+	// Add 1 cost if lift between floors:
 	floor := elev.GetFloor()
 	if floor == -1 {
 		cost++
 		floor = fsm.GetFloor()
 
+		// Find next floor:
 		switch fsm.GetDirection() {
 		case elev.DirnDown:
 			floor--
@@ -27,6 +27,7 @@ func calculateCost(targetFloor int, targetDirection elev.DirnType) {
 		}
 	}
 
+	// Loop through floors until target found, and accumulate cost:
 	direction = fsm.GetDirection()
 	for floor != targetFloor && direction != targetDirection {
 		if floor <= 0 {
@@ -39,19 +40,11 @@ func calculateCost(targetFloor int, targetDirection elev.DirnType) {
 			floor += direction
 		}
 
-		cost += 2
-		if shouldStop() {
+		if queue.ShouldStop(floor, direction) {
 			cost += 2
 		}
+		cost += 2
 	}
 
 	return cost
-
-	// finn antall stopp før ankomst til newOrder:
-	// finn antall etasjer å kjøre før ankomst til newOrder:
-	// (pass på at det tas høyde for retningen heisen evt. kjører)
-}
-
-func shouldStop() {
-	
 }
