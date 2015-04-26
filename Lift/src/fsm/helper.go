db9eed563eb01e36493e82d9e10a25b334d562a3
Morten Fyhn Amundsen <morten.fyhn.amundsen@gmail.com>
2015-04-26 23:33:26 +0200

    Documentation

--- Lift/src/fsm/helper.go
+++ Lift/src/fsm/helper.go
@@ -1,12 +1,12 @@
 package fsm
 
-// Direction returns the direction as stored in the state machine.
+// Direction returns the current direction as stored in the state machine.
 func Direction() int {
 	return dir
 }
 
-// Floor returns the floor as stored in the state machine.
-// If the lift is between floors, it returns the previous floor.
+// Floor returns the current floor as stored in the state machine.
+// If the lift is between floors, it returns the most recent floor.
 func Floor() int {
 	return floor
 }
