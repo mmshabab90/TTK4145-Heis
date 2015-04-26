83bee5e53022ff3928464379a7885d2741343b20
Morten Fyhn Amundsen <morten.fyhn.amundsen@gmail.com>
2015-04-26 02:55:34 +0200

    Cleanup main.go

--- Lift/src/main/main.go
+++ Lift/src/main/main.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	def "config"
-	"errors"
-	"fmt"
 	"fsm"
 	"hw"
 	"liftAssigner"
@@ -15,12 +13,8 @@ import (
 	"time"
 )
 
-var _ = log.Println
-var _ = fmt.Println
-var _ = errors.New
-
 var onlineLifts = make(map[string]network.UdpConnection)
-var numberOfOnlineLifts int
+var numOnline int
 
 var deadChan = make(chan network.UdpConnection)
 var costChan = make(chan def.Message)
@@ -48,13 +42,12 @@ func main() {
 
 	network.Init(outgoingMsg, incomingMsg)
 
-	go liftAssigner.Run(costChan, &numberOfOnlineLifts)
+	go liftAssigner.Run(costChan, &numOnline)
 	go eventHandler(ch)
 	go syncLights()
 
 	queue.Init(ch.NewOrder, outgoingMsg)
 
-	// Handle CTRL+C
 	go safeKill()
 
 	balboa := make(chan bool)
@@ -67,15 +60,12 @@ func eventHandler(ch fsm.Channels) {
 
 	for {
 		select {
-		case keypress := <-buttonChan:
-			switch keypress.Button {
+		case key := <-buttonChan:
+			switch key.Button {
 			case def.BtnInside:
-				queue.AddLocalOrder(keypress.Floor, keypress.Button)
+				queue.AddLocalOrder(key.Floor, key.Button)
 			case def.BtnUp, def.BtnDown:
-				outgoingMsg <- def.Message{
-					Category: def.NewOrder,
-					Floor:    keypress.Floor,
-					Button:   keypress.Button}
+				outgoingMsg <- def.Message{Category: def.NewOrder, Floor: key.Floor, Button: key.Button}
 			}
 		case floor := <-floorChan:
 			ch.FloorReached <- floor
@@ -84,7 +74,7 @@ func eventHandler(ch fsm.Channels) {
 		case connection := <-deadChan:
 			handleDeadLift(connection.Addr)
 		case order := <-queue.OrderTimeoutChan:
-			log.Println(def.ColR, "Order timeout, I can do it myself!", def.ColN)
+			log.Println(def.ColR, "Order timeout, I will do it myself!", def.ColN)
 			queue.RemoveRemoteOrdersAt(order.Floor)
 			queue.AddRemoteOrder(order.Floor, order.Button, def.Laddr)
 		case dir := <-ch.MotorDir:
@@ -99,7 +89,6 @@ func eventHandler(ch fsm.Channels) {
 
 func pollButtons() <-chan def.Keypress {
 	c := make(chan def.Keypress)
-
 	go func() {
 		var buttonState [def.NumFloors][def.NumButtons]bool
 
@@ -123,13 +112,11 @@ func pollButtons() <-chan def.Keypress {
 			time.Sleep(time.Millisecond)
 		}
 	}()
-
 	return c
 }
 
 func pollFloors() <-chan int {
 	c := make(chan int)
-
 	go func() {
 		oldFloor := hw.Floor()
 
@@ -142,77 +129,65 @@ func pollFloors() <-chan int {
 			time.Sleep(time.Millisecond)
 		}
 	}()
-
 	return c
 }
 
-func handleMessage(message def.Message) { // consider moving each case into a function
+// handleMessage handles incoming messages from the network.
+func handleMessage(msg def.Message) {
 	const aliveTimeout = 2 * time.Second
 
-	switch message.Category {
+	switch msg.Category {
 	case def.Alive:
-		if connection, exist := onlineLifts[message.Addr]; exist {
+		if connection, exist := onlineLifts[msg.Addr]; exist {
 			connection.Timer.Reset(aliveTimeout)
 		} else {
-			newConnection := network.UdpConnection{message.Addr, time.NewTimer(aliveTimeout)}
-			onlineLifts[message.Addr] = newConnection
-			numberOfOnlineLifts = len(onlineLifts)
+			newConnection := network.UdpConnection{msg.Addr, time.NewTimer(aliveTimeout)}
+			onlineLifts[msg.Addr] = newConnection
+			numOnline = len(onlineLifts)
 			go connectionTimer(&newConnection)
-			log.Printf("%sConnection to IP %s established!%s", def.ColG, message.Addr[0:15], def.ColN)
+			log.Printf("%sConnection to IP %s established!%s", def.ColG, msg.Addr[0:15], def.ColN)
 		}
 	case def.NewOrder:
-		// log.Printf("handleMessage(): NewOrder message: f=%d b=%d from lift %s\n",
-		//	message.Floor+1, message.Button, message.Addr[12:15])
-
-		cost := queue.CalculateCost(message.Floor, message.Button, fsm.Floor(), hw.Floor(), fsm.Direction())
-
-		costMessage := def.Message{
-			Category: def.Cost,
-			Floor:    message.Floor,
-			Button:   message.Button,
-			Cost:     cost}
-		// log.Printf("handleMessage(): NewOrder sends cost message: f=%d b=%d (with cost %d) from me\n", costMessage.Floor+1, costMessage.Button, costMessage.Cost)
-		outgoingMsg <- costMessage
+		cost := queue.CalculateCost(msg.Floor, msg.Button, fsm.Floor(), hw.Floor(), fsm.Direction())
+		outgoingMsg <- def.Message{Category: def.Cost, Floor: msg.Floor, Button: msg.Button, Cost: cost}
 	case def.CompleteOrder:
-		queue.RemoveRemoteOrdersAt(message.Floor)
+		queue.RemoveRemoteOrdersAt(msg.Floor)
 	case def.Cost:
-		// log.Printf("handleMessage(): Cost message: f=%d b=%d with cost %d from lift %s\n", message.Floor+1, message.Button, message.Cost, message.Addr[12:15])
-		costChan <- message
+		costChan <- msg
 	}
 }
 
-// handleDeadLift removes the lift that have timed out from the onlineLifts
-// and reassigns the dead lifts orders
+// handleDeadLift removes a dead lift from the list of online lifts, and
+// reassigns any orderes assigned to it.
 func handleDeadLift(deadAddr string) {
 	log.Printf("%sConnection to IP %s is dead!%s", def.ColR, deadAddr[0:15], def.ColN)
 	delete(onlineLifts, deadAddr)
-	numberOfOnlineLifts = len(onlineLifts)
+	numOnline = len(onlineLifts)
 	queue.ReassignOrders(deadAddr, outgoingMsg)
 }
 
-// connectionTimer is a go-routine for detecting that lifts aren't on the network anymore 1
+// connectionTimer finds out when any lifts are lost from the network.
 func connectionTimer(connection *network.UdpConnection) {
 	<-connection.Timer.C
 	deadChan <- *connection
 }
 
-// safeKill stops the motor if the program is killed with CTRL+C.
+// safeKill turns the motor off if the program is killed with CTRL+C.
 func safeKill() {
 	var c = make(chan os.Signal)
 	signal.Notify(c, os.Interrupt)
 	<-c
 	hw.SetMotorDir(def.DirStop)
-	log.Fatal(def.ColR, "User terminated program", def.ColN)
+	log.Fatal(def.ColR, "User terminated program.", def.ColN)
 }
 
+// syncLights checks the queues and updates all order lamps accordingly.
 func syncLights() {
 	for {
 		<-def.SyncLightsChan
-
 		for f := 0; f < def.NumFloors; f++ {
 			for b := 0; b < def.NumButtons; b++ {
-				if (b == def.BtnUp && f == def.NumFloors-1) ||
-					(b == def.BtnDown && f == 0) {
+				if (b == def.BtnUp && f == def.NumFloors-1) || (b == def.BtnDown && f == 0) {
 					continue
 				} else {
 					switch b {
